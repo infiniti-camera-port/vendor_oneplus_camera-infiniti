7c52b4faab38ef718ae80bcec8bb3fbea6a0c790
1vivy <1vivy@tutanota.com>
2026-07-08 18:30:50 -0600

    camera-infiniti: add root camera payload manifest rows

--- camera-infiniti.mk
+++ camera-infiniti.mk
@@ -6,3 +6,6 @@
 $(call inherit-product, vendor/oneplus/camera-sm8850-common/camera-sm8850-common.mk)
 
 $(call inherit-product, proprietary/vendor/oneplus/camera-infiniti/camera-infiniti-vendor.mk)
+
+PRODUCT_COPY_FILES += \
+    vendor/oneplus/camera-infiniti/configs/init/init.camera_process.rc:$(TARGET_COPY_OUT_ODM)/etc/init/init.camera_process.rc
